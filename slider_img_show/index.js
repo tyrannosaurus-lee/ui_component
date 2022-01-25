a1e1799a84ed9abd37bde2fa4a8af2cf5dbd9e26
tyrannosaurus-lee <miaow106@gmail.com>
2022-01-25 22:05:08 +0900

    image show 추가

--- slider_img_show/index.js
+++ slider_img_show/index.js
@@ -1,73 +1,57 @@
-// 1. 동작은 되지만 감시대상이 너무 많음
-/*
-const items = document.querySelectorAll('.item');
-items.forEach(function(item){
-    item.addEventListener('click', function(e){
-        item.classList.toggle('open');
-        items.forEach(function(elem){
-            if(elem !== item) elem.classList.remove('open');
-        })
-    })
-});
-*/
+const bigPic = document.querySelector("#cup");
+const smallPic = document.querySelectorAll(".small");
+let isOpen = false;
 
+for(let i=0; i<smallPic.length; i++){
+    // 작은 이미지 노드리스트들을 클릭을 하면  changePic()가 호출이 된다.
+    smallPic[i].addEventListener("click", changePic);
+}
 
+function changePic(){
+    // click이벤트가 발생한 대상(this)의 src속성을 newPic에 저장
+    let newPic = this.src;
+    // newPic의 값을 큰 이미지의 src속성으로 대입이 되어 큰 이미지로 나타남
+    bigPic.setAttribute("src", newPic);
+}
 
-// 2. 이벤트리스너 최소화화기
-/*
-const wrapper = document.querySelector('.wrapper');
-const items = document.querySelectorAll('.item');
-// 클릭이벤트가 버블링이 이루어진다.
-// wrapper가 상위dom이고 item은 하위dom
-// 하위dom에서 click event가 발생하면 그 이벤트를 상위로 전파함
-// 전파하다보니 wrapper로 전달됨.
-// wrapper에는 클릭이벤트에 대한 리스터가 등록되어 있기 때문에 실행됨
-// 실행은 됐지만 실제 클릭이 발생한 대상(event target)은 아이템으로 잡혀있기 때문에
-// target elem이 item인 경우에 걸러짐
-wrapper.addEventListener('click', function(e){
-    const targetElem = e.target;
-    console.dir(e);
-    // 버블링에 의해 wrappper의 상위dom인 body.addEventListener까지 영향을 미치므로 stopProgpagation을 준다.
-    e.stopPropagation();
-    if(!targetElem.classList.contains('item')) return;
-    targetElem.classList.toggle('open');
-    items.forEach(function(elem){
-        if(elem !== targetElem) elem.classList.remove('open');
-    })
-})
+const pic = document.querySelector("#pic");
+// id선택자인 pic에게 마우스가 올라가면 changePic()를 호출하고, false는 버블링을 하지 않겠다는 뜻
+// 버블링이란 이벤트가 하위요소에서 발생하면 이게 상위요소까지 전파되는 형태
+pic.addEventListener("mouseover", changePic, false);
+pic.addEventListener("mouseout", originPic, false);
 
-// 팝 오버 컴포넌트
-document.body.addEventListener('click', function(e){
-    // context 열려있는 상태에서는 동작을 하면 안됨
-    if(e.target.classList.contains('context')) return;
-    // 그 외에는 items의 open 클래스를 지워준다
-    // 목록 이외의 어떤 부분을 선택하면 item에 있는 open 클래스를 전부 지워준다.
-    items.forEach(function(elem){
-        elem.classList.remove('open');
-    })
-})
-*/
+function changePic(){
+    pic.src = "img/boy.png";
+}
 
+function originPic(){
+    pic.src = "img/girl.png";
+}
 
-
-// 3. 클릭 이벤트 하나로 줄이기
-const wrapper = document.querySelector('.wrapper');
-const items = document.querySelectorAll('.item');
-// body하나에만 클릭이벤트를 주고 context,item,그 외에 해당하는지에 따라서 처리를 다르게 한다.
-// 장점 : 이벤트리스너가 줄어듦
-// 단점 : (클릭이벤트 하나에 대하여)조건문들이 많아짐
-document.body.addEventListener('click', function(e){
-    const targetClassList = e.target.classList;
-    // targetClassList가 context이면 아무동작도 하지 않는다.
-    if(targetClassList.contains('context')) return;
-    if(targetClassList.contains('item')) {
-        targetClassList.toggle('open');
-        items.forEach(function(elem){
-            if(elem !== e.target) elem.classList.remove('open');
-        });
-        return;
+const view = document.querySelector("#view");
+view.addEventListener("click", function(){
+    // 상세 정보가 감춰져 있다면
+    if(isOpen == false){
+        // 상세 정보를 보이기
+        document.querySelector("#detail").style.display = "block";
+        view.innerHTML = "상세 설명 닫기";
+        isOpen = true;
+    }else{
+        document.querySelector("#detail").style.display = "none";
+        view.innerHTML = "상세 설명 보기";
+        isOpen = false;
     }
-    items.forEach(function(elem) {
-        elem.classList.remove('open');
-    });
 })
+
+/* 도형의 변환 */
+const mRect = document.querySelector("#rect");
+mRect.addEventListener("mouseover", function(){
+    mRect.style.background = "red";
+    mRect.style.borderRadius = "50%";
+});
+
+mRect.addEventListener("mouseout", function(){
+    mRect.style.background = "";
+    mRect.style.borderRadius = "";
+})
+/* //도형의 변환 */
