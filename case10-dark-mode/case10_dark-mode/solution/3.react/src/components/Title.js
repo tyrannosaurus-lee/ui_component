feaed51f89a8cf21d6aee0ab1b8c55ddc0dddce0
tyrannosaurus-lee <miaow106@gmail.com>
2022-02-16 15:02:51 +0900

    파일 정리(삭제)

--- case10-dark-mode/case10_dark-mode/solution/3.react/src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  color: #db5b33;
-  font-weight: 300;
-  text-align: center;
-`;
-
-export default Title;
--- case10-dark-mode/case10_dark-mode/solution/3.react/src/components/index.js
+++
@@ -1,4 +0,0 @@
-// re-export default import as named export
-export { default as Title } from './Title';
-export { default as ToggleButton } from './ToggleButton';
-export { default as Article } from './Article';
